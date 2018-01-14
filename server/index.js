af8fc00acabb5dd930e780e8cc3efd8bef0a7d89
sw-yx <swyx@swyx.io>
2018-01-15 01:12:25 +0800

    get ready for heroku

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000; // process.env.PORT is useful for heroku deploy
+const path = require("path");
 
 app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Expres2s" });
 });
+
+app.use(express.static(path.join(__dirname, "../client/build/")));
+
+// /* this is the catchall to deliver index.html */
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../client/public/index.html'));
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
