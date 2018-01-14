4cb2fecbe4e23c792190386e276618bee1223bb5
sw-yx <swyx@swyx.io>
2018-01-15 00:53:27 +0800

    clean basic sunburst

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { graphql } from "react-apollo";
-import gql from "graphql-tag";
-
-// Example query from https://www.graph.cool/
-const MOVIE_QUERY = gql`
-  {
-    Movie(id: "cixos5gtq0ogi0126tvekxo27") {
-      id
-      title
-      actors {
-        name
-      }
-    }
-  }
-`;
-
 class App extends Component {
   state = {
     response: ""
@@ -39,13 +23,6 @@ class App extends Component {
   };
 
   render() {
-    const { data } = this.props;
-    const { loading, Movie } = data;
-
-    // Loading
-    if (loading) return <div>loading...</div>;
-
-    // Loaded
     return (
       <div className="App">
         <header className="App-header">
@@ -53,13 +30,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">API Response: {this.state.response}</p>
-        <p>
-          <b>{Movie.title}</b> :{" "}
-          {Movie.actors.map(({ name }) => name).join(", ")}
-        </p>
       </div>
     );
   }
 }
 
-export default graphql(MOVIE_QUERY)(App);
+export default App;
