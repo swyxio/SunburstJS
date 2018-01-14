87761535e209515e9ff96cc313ab5883dc3c166f
sw-yx <swyx@swyx.io>
2018-01-15 02:13:50 +0800

    added mongoose

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    response: ""
+    response: "",
+    counter: 0
   };
 
   componentDidMount() {
@@ -16,13 +17,29 @@ class App extends Component {
   callApi = async () => {
     const response = await fetch("/api/hello");
     const body = await response.json();
-
     if (response.status !== 200) throw Error(body.message);
-
+    await this.getClick();
     return body;
   };
 
+  clickHandler = async () => {
+    await fetch("/api/click", {
+      method: "post",
+      body: JSON.stringify({ counter: this.state.counter + 1 }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+    this.getClick();
+  };
+  getClick = async () => {
+    const clicks = await fetch("/api/click").then(res => res.json());
+    this.setState({ counter: clicks });
+  };
+  resetHandler = () => fetch("/api/reset").then(this.getClick);
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
@@ -30,6 +47,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">API Response: {this.state.response}</p>
+        <h1>Counter</h1>
+        <p>Sunburst.js</p>
+        <p>You have clicked the button {this.state.counter} times.</p>
+        <button onClick={this.clickHandler}>Click Me!</button>
+        <button onClick={this.resetHandler}>Reset!</button>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -4,9 +4,46 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000; // process.env.PORT is useful for heroku deploy
 const path = require("path");
+const mongo = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/sunburstjs", { useMongoClient: true }); // :27017
+mongoose.Promise = global.Promise;
+
+var ClickModel = mongoose.model("ClickCounter", { counter: Number });
+
+// mongo.connect("mongodb://localhost:27017/sunburstjs", function(err, db) {
+//   if (err) throw new Error("Database failed to connect!");
+//   else console.log("MongoDB successfully connected on port 27017.");
+
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Expres2s" });
+});
+
+app.post("/api/click", async (req, res) => {
+  console.log("POST api/click", req.body);
+  var newUpdate = new ClickModel({ counter: req.body.counter });
+  await newUpdate.save(function(err, updatedCounter) {
+    if (err) return console.error(err);
+    res.json({ updatedCounter });
+  });
+});
+app.get("/api/reset", (req, res) => {
+  console.log("GET api/reset");
+  ClickModel.deleteMany().then(res.send(200));
+});
+app.get("/api/click", async (req, res) => {
+  console.log("GET api/click");
+  await ClickModel.find(function(err, counter) {
+    if (err) return console.error(err);
+    if (counter) res.json(counter.length);
+    else res.json(0);
+  });
 });
 
 app.use(express.static(path.join(__dirname, "../client/build/")));
